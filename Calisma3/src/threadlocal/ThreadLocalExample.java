225bf8028eb43f645c1d4173e1819ce2effdfc8b
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2020-10-22 14:28:23 +0300

    localdatetime eklendi

--- Calisma3/src/threadlocal/ThreadLocalExample.java
+++ Calisma3/src/threadlocal/ThreadLocalExample.java
@@ -1,24 +1,29 @@
 package threadlocal;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class ThreadLocalExample {
 
 	public static class MyRunnable implements Runnable {
 
-		// threadlocal farklı threadlerde ezilmezken, otherDegisken eziliyor..
+		// threadlocal farklı threadlerde ezilmezken, otherDegisken ve localDateTime  eziliyor.. ( sleep süresi değişkenlerin ezilmesini garantiliyor)
 		private ThreadLocal<Integer> threadLocal = new ThreadLocal<Integer>();
 		private Integer otherDegisken;
+		private LocalDateTime localDateTime;
 
 		@Override
 		public void run() {
 			threadLocal.set(Integer.valueOf(Thread.currentThread().getId() + ""));
 			otherDegisken = Integer.valueOf(Thread.currentThread().getId() + "");
+			localDateTime = LocalDateTime.now();
 
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 			}
 
-			System.out.println(Thread.currentThread().getId() + " --> " + threadLocal.get() + " " + otherDegisken);
+			System.out.println(Thread.currentThread().getId() + " --> " + threadLocal.get() + " " + otherDegisken + " " + localDateTime);
 		}
 	}
 
@@ -27,13 +32,16 @@ public class ThreadLocalExample {
 
 		Thread thread1 = new Thread(sharedRunnableInstance);
 		Thread thread2 = new Thread(sharedRunnableInstance);
+		Thread thread3 = new Thread(sharedRunnableInstance);
 
 		thread1.start();
 		thread2.start();
+		thread3.start();
 
 		try {
 			thread1.join(); // wait for thread 1 to terminate
 			thread2.join(); // wait for thread 2 to terminate
+			thread3.join(); // wait for thread 2 to terminate
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
