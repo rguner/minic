76063d5438a173cb6d5ad9a9daba8d47820009e6
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2020-10-26 13:19:06 +0300

    stream calisma

--- java8calisma/src/main/java/com/guner/java8calisma/StreamCalisma.java
+++ java8calisma/src/main/java/com/guner/java8calisma/StreamCalisma.java
@@ -1,12 +1,10 @@
 package com.guner.java8calisma;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class StreamCalisma {
 
@@ -19,6 +17,10 @@ public class StreamCalisma {
         s.execute5();
         s.execute6();
         s.execute7();
+        s.execute8();
+        s.execute9();
+        s.execute10();
+        s.execute11();
     }
 
     private void execute1() {
@@ -75,9 +77,85 @@ public class StreamCalisma {
     }
 
     private void execute6() {
+
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        List names = Arrays.asList("Ali", "Veli", "Selami", "Cem");
+
+        Stream<String> stream = names.stream();
+        Stream upperStream = stream.map(name -> name.toUpperCase());
+        upperStream.forEach(System.out::println);
+        //List upperNames = (List) upperStream.collect(Collectors.toList());
+        //upperNames.forEach(System.out::println);
     }
 
     private void execute7() {
+
+        System.out.println("---------------------------------------------------------------------------------------");
+        IntStream
+                .rangeClosed(1, 5)
+                .map(n -> n * n)
+                .forEach(System.out::println);
+        ;
+    }
+
+    private void execute8() {
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        int result = IntStream
+                .of(1, 2, 3, 4, 5)
+                .reduce(0, (once, sonra) -> {
+                    System.out.format("%d - %d %n", once, sonra);
+                    return once + sonra;
+                });
+        System.out.println(result);
+    }
+
+    private void execute9() {
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        int result = IntStream
+                .of(1, 2, 3, 4, 5)
+                .reduce(1, Math::multiplyExact);
+        System.out.println(result);
+    }
+
+    private void execute10() {
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        List<Integer> ints = Arrays.asList(1, 5, 3, 7, 11, 9, 15, 13);
+
+        ints.parallelStream() // Paralel Stream
+                .filter(Objects::nonNull) // null değilse
+                .filter(n -> n > 0) // pozitif sayı ise
+                .sorted() // sırala
+                .forEach(System.out::println); // çıktıla
+    }
+
+    private void execute11() {
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        List<Integer> names = Arrays.asList(1, 2, 3, 6, 7, 8, 9, null);
+
+        // lazy
+        Stream stream = names
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(n -> n % 2 == 1)
+                .map(n -> n * 2);
+        //Örneğin yukarıdaki liste üzerinde yapılmak istenen 2 filter ve 1 map işlemi Lazy işlemlerdir. Kod parçası bu haliyle çalıştırıldığında ne bir filtreleme ne de bir dönüştürme işlemi yapılacaktır. Burada yapılan sadece Stream nesnesini hazırlamaktır. Lazy işlemler gerekmedikçe işleme konulmamaktadır.
+
+        List<Integer> names2 = Arrays.asList(1, 2, 3, 6, 7, 8, 9, null);
+
+        //lazy
+        Stream stream2 = names2
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(n -> n % 2 == 1)
+                .map(n -> n * 2);
+
+        // eager
+        stream2.forEach(System.out::println); // Fakat bu hazırlanan Stream nesnesi, yukarıdaki gibi bir Eager operasyonla karşılaşırsa, önceki zincirlerde biriken Lazy işlemleri de harekete geçirecektir. Yani bu işlem, yukarıdak işlemleri tetikler
     }
 }
 
