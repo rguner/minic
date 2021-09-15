b728faf49e729b97fe82617f70ea37bb99268fc4
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-09-15 13:42:08 +0300

    clone

---
+++ Calisma/src/Clonable.java
@@ -0,0 +1,9 @@
+public class Clonable implements Cloneable{
+    private String s1;
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+}
--- Calisma/src/NullCalisma.java
+++ Calisma/src/NullCalisma.java
@@ -8,6 +8,8 @@ public class NullCalisma {
         NullCalisma nullCalisma = new NullCalisma();
         nullCalisma.execute();
         nullCalisma.execute2();
+        nullCalisma.execute3();
+        nullCalisma.execute4();
     }
 
     private void execute() {
@@ -25,6 +27,29 @@ public class NullCalisma {
             throw new NullPointerException("null oluştu");
         } catch (Exception e) {
             System.out.println("Exception oluştu" + e);
+        }
+    }
+
+    private void execute3() {
+
+        Object o = new String("ramazan");
+        String s= (String)o;
+        System.out.println(s);
+
+        Object o2 = null;
+        String s2= (String)o2;
+        System.out.println(s2);
+    }
+
+    private void execute4() {
+        //Clonable c3 = new Clonable();
+        Clonable c3 = null;
+        try {
+            c3.clone();
+            System.out.println("cloned");
+        }catch (Exception e) {
+            System.out.println("clone exception");
+            e.printStackTrace();
         }
     }
 }
