578c679c25b9f0d6122ecb49b0c493ee11b69310
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-09-15 12:21:19 +0300

    null denemesi

--- Calisma/src/NullCalisma.java
+++ Calisma/src/NullCalisma.java
@@ -3,21 +3,28 @@ import java.util.List;
 
 
 public class NullCalisma {
-	
-	public static void main(String[] args) {
-		NullCalisma nullCalisma = new NullCalisma();
-		nullCalisma.execute();
-	}
 
-	private void execute() {
-		 List<Object> values = new ArrayList<>();
-		 
-		 //values=null;
-		 for (Object object : values) {
-			 System.out.println("deneme");
-		 }
-		 
-		
-	}
+    public static void main(String[] args) {
+        NullCalisma nullCalisma = new NullCalisma();
+        nullCalisma.execute();
+        nullCalisma.execute2();
+    }
 
+    private void execute() {
+        List<Object> values = new ArrayList<>();
+
+        //values=null;
+        for (Object object : values) {
+            System.out.println("deneme");
+        }
+
+    }
+
+    private void execute2() {
+        try {
+            throw new NullPointerException("null oluştu");
+        } catch (Exception e) {
+            System.out.println("Exception oluştu" + e);
+        }
+    }
 }
