a82c872b677ae2a9362f206b9023796aa31b3d9b
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-09-17 10:38:53 +0300

    stream map

--- karalama/src/main/java/com/guner/ListMulakat.java
+++ karalama/src/main/java/com/guner/ListMulakat.java
@@ -2,11 +2,23 @@ package com.guner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class ListMulakat {
 
     public static void main(String[] args) {
+
+        ListMulakat listMulakat = new ListMulakat();
+        listMulakat.asList1();
+        listMulakat.asList2();
+        listMulakat.removeItemInIterator();
+
+
+
+    }
+
+    private void asList1() {
         // Arrays.asList, mevcut objeyi kullanarak list yapar, set işlemi arrayi değiştirir.
         String[] stringArray = new String[] {"A","B","C","D"};
         List<String> stringList = Arrays.asList(stringArray);
@@ -16,9 +28,12 @@ public class ListMulakat {
         for (String s : stringArray) {
             System.out.println(s);
         }
+    }
 
+    private void asList2() {
         // Arrays.asList, mevcut objeyi kullanarak list yapar, set işlemi arrayi değiştirir. bu nedenle new ArrayList faydalı olabilir.
         System.out.println("#######################################################");
+        String[] stringArray = new String[] {"A","B","C","D"};
         String[] stringArray2 = new String[] {"A","B","C","D"};
         List<String> stringList2 = new ArrayList<>(Arrays.asList(stringArray));
         stringList2.set(0,"E");
@@ -27,7 +42,27 @@ public class ListMulakat {
         for (String s : stringArray2) {
             System.out.println(s);
         }
+    }
 
+    private void removeItemInIterator() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
 
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            Integer value = it.next();
+            /*
+            if (value==3) {
+                list.remove(3);
+            }
+            */
+            list.remove(4);
+
+            System.out.println(value);
+        }
     }
 }
--- karalama/src/main/java/com/guner/Mulakat.java
+++ karalama/src/main/java/com/guner/Mulakat.java
@@ -9,31 +9,9 @@ public class Mulakat {
         Mulakat m = new Mulakat();
         m.integerCompare();
         m.stringCompare();
-        //m.removeItemInIterator();
         m.streamMap();
-    }
-
-
-    private void removeItemInIterator() {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-
-        Iterator<Integer> it = list.iterator();
-        while (it.hasNext()) {
-            Integer value = it.next();
-            /*
-            if (value==3) {
-                list.remove(3);
-            }
-            */
-            list.remove(4);
-
-            System.out.println(value);
-        }
+        m.streamFilter();
+        m.callFoo();
     }
 
     private void integerCompare() {
@@ -62,17 +40,120 @@ public class Mulakat {
         System.out.println("Sevval --> " + (s5 == s6));
         System.out.println("Sevval Equals--> " + (s5.equals(s6)));
 
+        System.out.println("---------------------------------------------------------------------------------------");
+
     }
 
+    // SORU1
     private void streamMap() {
 
         Integer integer = 20;
         String string = "stringvalue";
         HashMap<String, String> hashMap = new HashMap();
         hashMap.put("key", "value");
-        List < Object > list = Arrays.asList("Sonuç", new Integer(20), string, hashMap);
+        List<Object> list = Arrays.asList("Sonuç", new Integer(20), string, hashMap);
         String resultString = list.stream().map(String::valueOf).collect(Collectors.joining("|"));
         System.out.println(resultString);
+        System.out.println("---------------------------------------------------------------------------------------");
     }
 
+    // SORU2
+    public class Person {
+
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.setName(name);
+            this.setAge(age);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [name=" + name + ", age=" + age + "]";
+        }
+    }
+
+    private void streamFilter() {
+        List<Person> persons = Arrays.asList(
+                new Person("Ahmet", 30),
+                new Person("Mehmet", 20),
+                new Person("Murat", 40));
+
+        Person result1 = persons.stream()
+                .filter(x -> "Mehmet".equals(x.getName()))
+                .findAny()
+                .orElse(null);
+
+        System.out.println(result1);
+
+        Person result2 = persons.stream()
+                .filter(x -> "Ayşe".equals(x.getName()))
+                .findAny()
+                .orElse(null);
+
+        System.out.println(result2);
+
+        System.out.println("---------------------------------------------------------------------------------------");
+
+    }
+
+
+    // SORU3
+
+    public class Futbolcu {
+
+        private String name;
+
+        Futbolcu(String name) {
+            this.name = name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Futbolcu :" + name;
+        }
+    }
+
+    public void method1(Futbolcu futbolcu) {
+        Futbolcu futbolcuAli = new Futbolcu("Ali");
+        futbolcu = futbolcuAli;
+    }
+
+    public void method2(Futbolcu futbolcu) {
+        futbolcu.setName("Feyyaz");
+    }
+
+    private void callFoo() {
+        Futbolcu futbolcuMetin = new Futbolcu("Metin");
+        method1(futbolcuMetin);
+        System.out.println(futbolcuMetin);
+
+        Futbolcu futbolcuMehmet = new Futbolcu("Mehmet");
+        method2(futbolcuMehmet);
+        System.out.println(futbolcuMehmet);
+
+        System.out.println("---------------------------------------------------------------------------------------");
+
+    }
 }
