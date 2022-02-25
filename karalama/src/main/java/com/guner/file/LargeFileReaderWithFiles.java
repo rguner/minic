513adce29d943f82a8435085fdbf8ed4194a2277
Ramazan Guner <rguner@gmail.com>
2022-02-25 17:25:02 +0300

    war file example

--- karalama/src/main/java/com/guner/file/LargeFileReaderWithFiles.java
+++ karalama/src/main/java/com/guner/file/LargeFileReaderWithFiles.java
@@ -1,7 +1,13 @@
 package com.guner.file;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,15 +15,22 @@ import java.nio.file.Paths;
 public class LargeFileReaderWithFiles {
 
     public static void main(String[] args) throws IOException {
-        String currentDirectory = System.getProperty("user.dir");
-        String file = currentDirectory + "\\file\\read\\file.txt";
-        Path path = Paths.get(file);
-        BufferedReader bufferedReader = Files.newBufferedReader(path);
+        LargeFileReaderWithFiles l = new LargeFileReaderWithFiles();
+        l.execute();
+    }
 
-        String curLine;
-        while ((curLine = bufferedReader.readLine()) != null){
-            System.out.println(curLine);
+    private void execute() {
+
+
+        try {
+            //InputStream in = this.getClass().getClassLoader().getResourceAsStream("file/read/StarWars3.wav");
+            URL url = getClass().getResource("/file/read/StarWars3.wav");
+            InputStream in = url.openStream();
+            AudioInputStream stream = AudioSystem.getAudioInputStream(in);
+            System.out.println(stream);
+            System.out.println(this.getClass().getClassLoader().getResourceAsStream("file/read/StarWars3.wav"));
+        } catch(Exception e) {
+            e.printStackTrace();
         }
-        bufferedReader.close();
     }
 }
