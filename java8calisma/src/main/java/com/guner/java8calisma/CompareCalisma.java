001997c05950ddca1f84a4f0ed81b5c1a56730de
Ramazan Guner <rguner@gmail.com>
2022-03-14 23:05:28 +0300

    compare

---
+++ java8calisma/src/main/java/com/guner/java8calisma/CompareCalisma.java
@@ -0,0 +1,60 @@
+package com.guner.java8calisma;
+
+import com.guner.java8calisma.model.Human;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class CompareCalisma {
+
+    List<Human> humans;
+
+    CompareCalisma() {
+        humans = Arrays.asList(
+                new Human("Sarah", 10),
+                new Human("Jack", 12),
+                new Human("Ramazan", 46),
+                new Human("Ceyhan", 43)
+        );
+
+    }
+
+    public static void main(String[] args) {
+        CompareCalisma c = new CompareCalisma();
+        c.execute1();
+        c.execute2();
+        c.execute3();
+        c.execute4();
+    }
+
+
+    private void execute1() {
+        System.out.println("-----------------------------------------------------");
+        humans.sort((h1, h2) -> h1.getName().compareTo(h2.getName()));
+        humans.forEach(System.out::println);
+    }
+
+    private void execute2() {
+        System.out.println("-----------------------------------------------------");
+        humans.sort(Comparator.comparing(Human::getName));
+        humans.forEach(System.out::println);
+    }
+
+    private void execute3() {
+        System.out.println("-----------------------------------------------------");
+        humans.sort(Comparator.comparing(Human::getAge).reversed());
+        humans.forEach(System.out::println);
+    }
+
+    private void execute4() {
+        System.out.println("-----------------------------------------------------");
+        humans.sort(
+                Comparator.comparing(Human::getName).thenComparing(Human::getAge)
+        );
+        humans.forEach(System.out::println);
+    }
+
+
+
+}
---
+++ java8calisma/src/main/java/com/guner/java8calisma/model/Human.java
@@ -0,0 +1,36 @@
+package com.guner.java8calisma.model;
+
+public class Human {
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
