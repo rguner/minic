70c4443111dfc09e1e04f6da2fddc9df4998003b
Ramazan Guner <rguner@gmail.com>
2022-03-19 00:37:17 +0300

    stack calisma

--- java8calisma/src/main/java/com/guner/java8calisma/StackCalisma.java
+++ java8calisma/src/main/java/com/guner/java8calisma/StackCalisma.java
@@ -20,18 +20,36 @@ public class StackCalisma {
         boolean result = intStack.addAll(intList);
         System.out.println("Sonuç: " + result);
         System.out.println("Stack size: " + intStack.size());
-        System.out.println("Search 3 : " + intStack.search(3));
-        System.out.println("Search 2 : " + intStack.search(2));
-        System.out.println("Search 1 : " + intStack.search(1));
-        System.out.println("Pop: " + intStack.pop());
-        System.out.println("Pop: " + intStack.pop());
-        System.out.println("Pop: " + intStack.pop());
-        System.out.println("Pop: " + intStack.pop());
-        System.out.println("Peek: " + intStack.peek());
-        System.out.println("Peek: " + intStack.peek());
+
         System.out.println("Search 3 : " + intStack.search(3));
         System.out.println("Search 2 : " + intStack.search(2));
         System.out.println("Search 1 : " + intStack.search(1));
 
+        System.out.println("Pop: " + intStack.pop());
+        System.out.println("Pop: " + intStack.pop());
+        System.out.println("Pop: " + intStack.pop());
+        System.out.println("Pop: " + intStack.pop());
+
+        System.out.println("Peek: " + intStack.peek());
+        System.out.println("Peek: " + intStack.peek());
+
+        System.out.println("Search 3 : " + intStack.search(3));
+        System.out.println("Search 2 : " + intStack.search(2));
+        System.out.println("Search 1 : " + intStack.search(1));
+
+        System.out.println("IndexOf 3 : " + intStack.indexOf(3));
+        System.out.println("IndexOf 2 : " + intStack.indexOf(2));
+        System.out.println("IndexOf 1 : " + intStack.indexOf(1));
+
+        System.out.println("Stack size "+ intStack.size() );
+        System.out.println("RemoveElement: " + intStack.removeElement(2) + " After removeElement, size "+ intStack.size() );
+        System.out.println("RemoveElement: " + intStack.removeElement(40) + " After removeElement, size "+ intStack.size() );
+
+        intStack.removeIf(element -> element < 6);
+        System.out.println("Stack size, after remove < 6 process "+ intStack.size() );
+
+        intStack.push(1);
+        intStack.addAll(intList);
+        intStack.forEach(System.out::print);
     }
 }
