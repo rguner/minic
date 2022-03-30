3336efdd936d43e78164691f03862ec4d6c9cab4
Ramazan Guner <rguner@gmail.com>
2022-03-30 17:53:04 +0300

    tree traversals

---
+++ karalama/src/main/java/com/guner/datastructure/binarytree/Node.java
@@ -0,0 +1,13 @@
+package com.guner.datastructure.binarytree;
+
+class Node {
+    int key;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        key = item;
+        left = right = null;
+    }
+}
+ 
