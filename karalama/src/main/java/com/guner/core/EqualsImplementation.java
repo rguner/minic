66d7f10424444cba41e94d2577b60a48ae138d1e
Ramazan Guner <rguner@gmail.com>
2022-04-01 15:45:16 +0300

    equals and hashcode

--- karalama/src/main/java/com/guner/core/EqualsImplementation.java
+++ karalama/src/main/java/com/guner/core/EqualsImplementation.java
@@ -7,16 +7,27 @@ class ComplexObject {
     private int a;
     private int b;
 
+    ComplexObject(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         ComplexObject that = (ComplexObject) obj;
-        return a==that.a;
+        return a == that.a;
     }
 
+    /*
     @Override
     public int hashCode() {
+        return Objects.hash(a, b);
+    }
+     */
+
+    public int hashCodeModified() {
         return Objects.hash(a, b);
     }
 
@@ -28,28 +39,32 @@ class ComplexObject {
         this.b = b;
     }
 }
+
 public class EqualsImplementation {
     public static void main(String[] args) {
         EqualsImplementation equalsImplementation = new EqualsImplementation();
-        equalsImplementation.deneme();
+        equalsImplementation.equalsDeneme();
+        equalsImplementation.hashCodeDeneme();
     }
 
-    private void deneme() {
-        ComplexObject c1= new ComplexObject();
-        c1.setA(100);
-        c1.setB(200);
+    private void hashCodeDeneme() {
+        ComplexObject c1 = new ComplexObject(100, 200);
+        ComplexObject c2 = new ComplexObject(100, 300);
+        ComplexObject c3 = new ComplexObject(100, 200);
+        System.out.println("c1 hashCode : " + c1.hashCode());
+        System.out.println("c2 hashCode : " + c2.hashCode());
+        System.out.println("c3 hashCode : " + c3.hashCode());
+        System.out.println("c1 hashCodeModified : " + c1.hashCodeModified());
+        System.out.println("c1 hashCodeModified : " + c2.hashCodeModified());
+        System.out.println("c3 hashCodeModified : " + c3.hashCodeModified());
+    }
 
-        ComplexObject c2= new ComplexObject();
-        c2.setA(100);
-        c2.setB(300);
+    private void equalsDeneme() {
+        ComplexObject c1 = new ComplexObject(100, 200);
+        ComplexObject c2 = new ComplexObject(100, 300);
+        ComplexObject c3 = new ComplexObject(800, 900);
 
-
-        ComplexObject c3= new ComplexObject();
-        c3.setA(800);
-        c3.setB(900);
-
-
-        System.out.println("c1 c2 Eşit -->" + c1.equals(c2));
-        System.out.println("c1 c3 Eşit -->" + c1.equals(c3));
+        System.out.println("c1 c2 Eşit --> " + c1.equals(c2));
+        System.out.println("c1 c3 Eşit --> " + c1.equals(c3));
     }
 }
