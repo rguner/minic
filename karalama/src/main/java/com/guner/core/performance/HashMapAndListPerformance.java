a9c24e3783e0223eb00a65544151cadf9ad9fd13
Ramazan Guner <rguner@gmail.com>
2022-04-22 15:12:11 +0300

    HashMap and List read compare

---
+++ karalama/src/main/java/com/guner/core/performance/HashMapAndListPerformance.java
@@ -0,0 +1,50 @@
+package com.guner.core.performance;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HashMapAndListPerformance {
+    public static void main(String[] args) {
+        HashMapAndListPerformance h = new HashMapAndListPerformance();
+        h.doItWithHashMap();
+        h.doItWithArrayList();
+    }
+
+    private final int PROCESS_COUNT = 800000;
+
+    private void doItWithArrayList() {
+
+        long t1 = System.currentTimeMillis();
+
+        List<String> list = Arrays.asList("Ramazan Güner0", "Ramazan Güner1", "Ramazan Güner2", "Ramazan Güner3", "Ramazan Güner4", "Ramazan Güner5");
+
+        for (int i = 0; i < PROCESS_COUNT; i++) {
+            for (int j = 0; j < list.size(); j++) {
+                String b = list.get(j);
+            }
+        }
+
+        System.out.println("ArrayList bitti : " + (System.currentTimeMillis() - t1));
+
+    }
+
+    private void doItWithHashMap() {
+
+        long t1 = System.currentTimeMillis();
+
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < 6; i++) {
+            map.put(i, "Ramazan Güner" + i);
+        }
+
+        for (int i = 0; i < PROCESS_COUNT; i++) {
+            for (int j = 0; j < map.size(); j++) {
+                String b = map.get(j);
+            }
+        }
+
+        System.out.println("HashMap bitti : " + (System.currentTimeMillis() - t1));
+    }
+}
