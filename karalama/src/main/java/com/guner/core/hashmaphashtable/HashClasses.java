5352f2e656c3f4b4a4e1f7c5694cb7df68596608
Ramazan Guner <rguner@gmail.com>
2022-05-24 19:33:42 +0300

    hashmap hashtable

--- karalama/src/main/java/com/guner/core/hashmaphashtable/HashClasses.java
+++ karalama/src/main/java/com/guner/core/hashmaphashtable/HashClasses.java
@@ -10,8 +10,11 @@ class HashClasses
     /*
     Hashmap vs Hashtable
 
-    HashMap is non-synchronized. It is not thread-safe and can’t be shared between many threads without proper synchronization code whereas Hashtable is synchronized. It is thread-safe and can be shared with many threads.
+    HashMap is non-synchronized. It is not thread-safe and can’t be shared between many threads without proper synchronization
+    code whereas Hashtable is synchronized. It is thread-safe and can be shared with many threads.
+
     HashMap allows one null key and multiple null values whereas Hashtable doesn’t allow any null key or value.
+
     HashMap is generally preferred over HashTable if thread synchronization is not needed.
      */
     public static void main(String args[])
