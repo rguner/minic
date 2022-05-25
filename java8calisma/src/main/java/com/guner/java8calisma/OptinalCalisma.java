42e20c2a6b475bb94dc2f1087ccbcb608cdeb640
Ramazan Guner <rguner@gmail.com>
2022-05-25 21:46:32 +0300

    optional çalisma

--- java8calisma/src/main/java/com/guner/java8calisma/OptinalCalisma.java
+++ java8calisma/src/main/java/com/guner/java8calisma/OptinalCalisma.java
@@ -19,6 +19,7 @@ public class OptinalCalisma {
 
 
     private void execute1() {
+        System.out.println("----------------------");
         Optional<String> opt = Optional.of("Ramazan");
         System.out.println("Optional.isEmpty : " + opt.isEmpty());
         System.out.println("Optional.isPresent : " + opt.isPresent());
@@ -27,27 +28,83 @@ public class OptinalCalisma {
     }
 
     private void execute2() {
+        System.out.println("----------------------");
+        String nullName = null;
+        String name = Optional.ofNullable(nullName).orElse("john");
+        System.out.println(name);
     }
 
     private void execute3() {
+        System.out.println("----------------------");
+        String nullName = null;
+        String name = Optional.ofNullable(nullName).orElseGet(() -> "john");
+        System.out.println(name);
     }
 
     private void execute4() {
+        System.out.println("----------------------");
+        try {
+            String nullName = null;
+            String name = Optional.ofNullable(nullName).orElseThrow(
+                    IllegalArgumentException::new);
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie);
+        }
     }
 
     private void execute5() {
+        System.out.println("----------------------");
+        try {
+            String nullName = null;
+            String name = Optional.ofNullable(nullName).orElseThrow();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     private void execute6() {
+        System.out.println("----------------------");
+        Integer year = 2016;
+        Optional<Integer> yearOptional = Optional.of(year);
+        boolean is2016 = yearOptional.filter(y -> y == 2016).isPresent();
+        System.out.println("is 2016 ? : " + is2016 );
+        boolean is2017 = yearOptional.filter(y -> y == 2017).isPresent();
+        System.out.println("is 2017 ? : " + is2017 );
     }
 
     private void execute7() {
+        System.out.println("----------------------");
+        Modem modem = new Modem(102D);
+        boolean isPresent = Optional.ofNullable(modem)
+                .map(Modem::getPrice)
+                .filter(p -> p >= 10)
+                .filter(p -> p <= 15)
+                .isPresent();
+        System.out.println("Modem price is betwwen 10 and 15 : " + isPresent);
     }
 
     private void execute8() {
+        System.out.println("----------------------");
     }
 
     private void execute9() {
+        System.out.println("----------------------");
+    }
+
+    public class Modem {
+        private Double price;
+
+        public Modem(Double price) {
+            this.price = price;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
+        }
     }
 
 }
