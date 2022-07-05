0d5b5e685e4ca2abd71ec2dbe4483b918abbe57c
Ramazan Guner <rguner@gmail.com>
2022-07-05 14:21:14 +0300

    simple clone

---
+++ Calisma/src/main/java/clone/DenemeClone.java
@@ -0,0 +1,55 @@
+package clone;
+
+import java.util.Objects;
+
+public class DenemeClone implements Cloneable{
+
+    private String ad;
+    private String soyad;
+    private int no;
+
+    public static void main(String[] args) {
+        DenemeClone denemeClone = new DenemeClone();
+        denemeClone.ad="remazan";
+        denemeClone.soyad="guner";
+        denemeClone.no=1300;
+        System.out.println(denemeClone + " " + denemeClone.hashCode());
+        try {
+            Object denemeClone2 = denemeClone.clone();
+            System.out.println(denemeClone2 + " " + denemeClone2.hashCode());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DenemeClone that = (DenemeClone) o;
+        return no == that.no && Objects.equals(ad, that.ad) && Objects.equals(soyad, that.soyad);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ad, soyad, no);
+    }
+
+     */
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "DenemeClone{" +
+                "ad='" + ad + '\'' +
+                ", soyad='" + soyad + '\'' +
+                ", no=" + no +
+                '}';
+    }
+}
