dee0b448ecc869ef32ceee2561cc7f5ff05213f2
Ramazan Guner <rguner@gmail.com>
2022-11-04 16:40:47 +0300

    SneakyException

--- karalama/src/main/java/com/guner/lombok/LombokDeneme.java
+++ karalama/src/main/java/com/guner/lombok/LombokDeneme.java
@@ -3,8 +3,10 @@ package com.guner.lombok;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 @Data
@@ -25,5 +27,23 @@ public class LombokDeneme {
     public static void main(String[] args) {
         LombokDeneme lombokDeneme = new LombokDeneme();
         lombokDeneme.logDeneme();
+        lombokDeneme.throwSneakyIOExceptionUsingLombok();
+        /*
+        try {
+            lombokDeneme.throwIOException();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+         */
     }
+
+    @SneakyThrows
+    public void throwSneakyIOExceptionUsingLombok() {
+        throw new IOException("lombok sneaky IOException");
+    }
+
+    public void throwIOException() throws IOException {
+        throw new IOException("Normal IOException");
+    }
+
 }
