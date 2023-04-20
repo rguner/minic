5987df639a411f7b50c74d1078a54f33ccb7f98d
Ramazan Guner <rguner@gmail.com>
2023-04-20 14:22:31 +0300

    immutable collection

--- karalama/src/main/java/com/guner/datastructure/colllection/CollectionRunner.java
+++ karalama/src/main/java/com/guner/datastructure/colllection/CollectionRunner.java
@@ -1,4 +1,5 @@
 package com.guner.datastructure.colllection;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -10,18 +11,18 @@ public class CollectionRunner {
     }
 
     private void createCollection() {
-        Room room1= Room.builder().name("Oda1").price(100).build();
-        Room room2= Room.builder().name("Oda2").price(200).build();
-        Room room3= Room.builder().name("Oda3").price(300).build();
+        Room room1 = Room.builder().name("Oda1").price(100).build();
+        Room room2 = Room.builder().name("Oda2").price(200).build();
+        Room room3 = Room.builder().name("Oda3").price(300).build();
 
-        Collection<Room> roomCollection = List.of(room1, room2);
+        Collection<Room> roomCollection = List.of(room1, room2, room3); // ImmutableCollection
         int totalPrice = getTotalPrices(roomCollection);
         System.out.printf("Total price : %d\n", totalPrice);
     }
 
     private int getTotalPrices(Collection<Room> roomCollection) {
         return roomCollection.stream()
-                .mapToInt(r->r.getPrice())
+                .mapToInt(r -> r.getPrice())
                 .sum();
 
 
